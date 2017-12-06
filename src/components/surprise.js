3a20729ad37e02e06b55e19555aaa85c5d9f8f74
vcordero07 <vcordero07@gmail.com>
2017-12-05 22:33:17 -0500

    wip

--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,24 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          display: 'button'
+        }
+    }
+    showImage() {
+      this.setState({
+        display: 'image'
+      });
     }
 
     render() {
         // Show the button to start with
-        return <SurpriseButton />;
+        if (this.state.display === 'button') {
+          return <SurpriseButton onClick={e => this.showImage()} />;
+        }
+        else if (this.state.display === 'image') {
+          return <SurpriseImage />;
+        }
+
     }
 }
