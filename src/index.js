c406aacec99364a88a521230ff31b9a2820f5bed
vcordero07 <vcordero07@gmail.com>
2017-12-05 22:12:29 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Surprise from './components/surprise';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Surprise />,
+  document.getElementById('root')
+);
